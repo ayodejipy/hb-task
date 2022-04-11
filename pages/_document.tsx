fbb173be3e50d17b71610376eace498e4a1c5efe
ayodejipy <ay.programmr@gmail.com>
2022-04-11 17:53:37 +0100

    fix - ctx typed to any

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ import Document, {
   } from 'next/document'
   
   class MyDocument extends Document {
-    static async getInitialProps(ctx) {
+    static async getInitialProps(ctx: any) {
       const initialProps = await Document.getInitialProps(ctx)
   
       return initialProps
